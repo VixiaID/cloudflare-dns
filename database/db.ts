c5b87124efde1295d7cd28f6e4b6723737c83b3f
VixiaID <otseeserver@yahoo.com>
2024-02-19 06:22:15 +0700

    postresql db

--- database/db.ts
+++ database/db.ts
@@ -1,36 +1,9 @@
 import { Sequelize } from 'sequelize';
-import fs from 'fs';
 
-let isDockerCached;
-
-const sequelize = new Sequelize({
-	dialect: 'sqlite',
-	storage: isDocker() ? '/db/database.sqlite' : './database.sqlite',
-	logging: false,
+const sequelize = new Sequelize(process.env.DB_URL, {
+  dialect: 'postgres',
+  dialectModule: require('pg'),
+  logging: false
 });
 
 export default sequelize;
-
-function hasDockerEnv() {
-	try {
-		fs.statSync('/.dockerenv');
-		return true;
-	} catch {
-		return false;
-	}
-}
-function hasDockerCGroup() {
-	try {
-		return fs.readFileSync('/proc/self/cgroup', 'utf8').includes('docker');
-	} catch {
-		return false;
-	}
-}
-function isDocker() {
-	// TODO: Use `??=` when targeting Node.js 16.
-	if (isDockerCached === undefined) {
-		isDockerCached = hasDockerEnv() || hasDockerCGroup();
-	}
-
-	return isDockerCached;
-}
